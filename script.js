314859dc7256a3488ae77fe54d8fff00517117ca
ckscode <cksiddhaarthan@gmail.com>
2024-04-23 18:59:35 +0530

    new changes

--- script.js
+++ script.js
@@ -8,56 +8,52 @@ function element(tag, classname, id, text) {
   }
 const Cont = element("div","container","","");
 const title = element("h1","text-center","title","");
-const Row = element("div","row","rows","");
-
+const Row = element("div","row" ,"rows","");
+title.innerHTML="Weather of countries"
 const res = fetch("https://restcountries.com/v3.1/all");
 
-title.innerHTML="Weather of countries"
-
-
-
-
-
+document.body.classList="bg-secondary"
 res.then((e)=>e.json()).then((item)=>{
 
-for(let i=0;i<=item.length;i++){
-   let lati;
-   let longi;
- if(item[i].capitalInfo.latlng){
-   lati =item[i].capitalInfo.latlng[0];
-  longi =item[i].capitalInfo.latlng[1];
- }
-   // console.log(lati,longi)
-   const Temp =element("div","temp","temp","");
-const Col = element("div","col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 g-2 text-center","","");
-const Card = element("div","card","","")
-const Cardtitle = element("div","card-header","name","");
-const Cardbody = element("div","card-body","","");
-const Image1 = element("img","card-img-top p-3","image","");
-const Weather = element("p","h-25","weather","&nbsp;");
-const Button = element("button","btn btn-primary","","Click for Weather");
-Button.addEventListener("click",()=>{
-  if(Weather.innerHTML === "&nbsp;"){
-    const location = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&units=metric&appid=fb764be4bfdca6671a8d9f259680c56e`);
-    location.then((e)=>e.json()).then((ele)=>{Weather.innerHTML=`${ele.main.temp}°C`}).catch((error)=>console.log(error))
-  }else{
-    Weather.innerHTML="&nbsp;";
-  }
- })
-// const latLong = (lat,long) 
-
-   Image1.src=item[i].flags.png
-   Cardtitle.innerHTML=`${item[i].name.common}`;
-   Cardbody.innerHTML = `
-   <p>Capital: ${item[i].capital}</p>
-   <p>Region: ${item[i].region}</p>
-   <p>Country Code: ${item[i].cca3}</p>`
-   Cardbody.append(Weather,Button);
-   Card.append(Cardtitle,Image1,Cardbody)
-   Col.append(Card);
+for(let i=0;i<item.length;i++){
+const Col = document.createElement("div");
+Col.classList = "col-sm-6 col-md-4 col-lg-4 col-xl-4 my-2"
+  Col.innerHTML=`
+  <div class="card h-100 text-center text-white bg-dark border-secondary">
+  <div class="card-header border-bottom border-secondary">
+  <h2 class="card-title">${item[i].name.common}</h2></div>
+  <div class="image-card p-3">
+  <img src="${item[i].flags.png}" class="card-img-top rounded-2 " alt="card image"/></div>
+  <div class="card-body pt-1">
+  <div class="card-text py-1 text-center">Region: ${item[i].region}</div>
+  <div class="card-text py-1 text-center">Capital: ${item[i].capital}</div>
+  <div class="card-text py-1 text-center">Country Code: ${item[i].cca3}</div>
+  <p id="weather" class="mb-0 weather">&nbsp;</p>
+  <button class="btn btn-primary mt-2 buttons" id="button">Click for Weather</button>
+  </div>
+  </div>
+  `
    Row.append(Col);
-
 }
+
+const Button1 = document.querySelectorAll(".buttons");
+
+Button1.forEach((e,index)=>{
+    e.addEventListener("click",()=>{
+      const Lati = Math.round(item[index].latlng[0]);
+      const Longi = Math.round(item[index].latlng[1]);
+      const Temp = document.querySelectorAll(".weather")
+      const rest = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${Lati}&lon=${Longi}&appid=fb764be4bfdca6671a8d9f259680c56e&units=metric`);
+      rest.then((e)=>e.json()).then((ele)=>{
+        if(Temp[index].innerHTML === "&nbsp;"){
+          Temp[index].innerHTML=`Temperature: ${ele.main.temp}°C`
+        }else{
+          Temp[index].innerHTML="&nbsp;";
+        }
+      }).catch(err=>console.log(err))
+    })
+})
+
 }).catch((error)=>console.log(error))
 Cont.append(title,Row);
 
